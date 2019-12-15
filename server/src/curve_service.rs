c7a9898e087f885715c4c9b2b6199dacf4067ba0
Hexilee <i@hexilee.me>
2019-12-16 01:57:55 +0800

    define index for curves

--- server/src/curve_service.rs
+++ server/src/curve_service.rs
@@ -6,14 +6,18 @@ use tokio::sync::mpsc;
 use tonic::{Request, Response, Status};
 
 pub struct CurveServiceImpl {
-    curve_sender: Arc<Mutex<Vec<mpsc::UnboundedSender<Result<Curve, Status>>>>>,
+    curve_senders: Arc<Vec<Mutex<Vec<mpsc::UnboundedSender<Result<Curve, Status>>>>>>,
 }
 
 impl CurveServiceImpl {
-    pub fn new(
-        curve_sender: Arc<Mutex<Vec<mpsc::UnboundedSender<Result<Curve, Status>>>>>,
-    ) -> Self {
-        Self { curve_sender }
+    pub fn new(curves_nums: usize) -> Self {
+        let mut curve_senders = Vec::with_capacity(curves_nums);
+        for _ in 0..curves_nums {
+            curve_senders.push(Mutex::new(Vec::new()))
+        }
+        Self {
+            curve_senders: Arc::new(curve_senders),
+        }
     }
 }
 
@@ -26,7 +30,10 @@ impl CurveService for CurveServiceImpl {
     ) -> Result<Response<Self::GetCurveStream>, Status> {
         println!("Got a request: {:?}", request);
         let (tx, rx) = mpsc::unbounded_channel();
-        self.curve_sender.lock().await.push(tx);
+        self.curve_senders[request.get_ref().index as usize]
+            .lock()
+            .await
+            .push(tx);
         Ok(Response::new(rx))
     }
 }
