65fe9cff12b006ac2dcaa71249cb8925b66399b2
Hexilee <i@hexilee.me>
2020-05-03 23:48:07 +0800

    remove serve dist
    Signed-off-by: Hexilee <i@hexilee.me>

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,21 +1,17 @@
 mod channels;
 mod curve;
 
-use async_std::path::{Path, PathBuf};
 use channels::mock::cos_channel;
 use channels::SyncChannel;
 use futures::{stream::SplitStream, SinkExt, StreamExt};
 use log::{debug, error, info, warn};
-use roa::body::DispositionType::Inline;
 use roa::cors::Cors;
-use roa::http::{Method, StatusCode};
 use roa::logger::logger;
 use roa::preload::*;
-use roa::router::{allow, get, Router};
 use roa::websocket::tungstenite::protocol::frame::{coding::CloseCode, CloseFrame};
 use roa::websocket::tungstenite::Error as WsError;
 use roa::websocket::{Message, SocketStream, Websocket};
-use roa::{throw, App, Context};
+use roa::{App, Context};
 use std::borrow::Cow;
 use std::env;
 
@@ -26,17 +22,10 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let server_addr = env::var("CROW_SERVER_ADDR")?;
     let channel = SyncChannel::new();
     cos_channel(channel.clone());
-    let router = Router::new()
-        .on("/", get(index))
-        .on(
-            "/ws",
-            allow([Method::GET], Websocket::new(handle_ws_client)),
-        )
-        .on("/*{file}", get(serve_sources));
     App::state(channel)
         .gate(logger)
         .gate(Cors::new())
-        .end(router.routes("/")?)
+        .end(Websocket::new(handle_ws_client))
         .listen(server_addr, |addr| info!("Server is listening on {}", addr))?
         .await?;
     Ok(())
@@ -77,28 +66,4 @@ async fn handle_message(mut receiver: SplitStream<SocketStream>) -> Result<(), W
         }
     }
     Ok(())
-}
-
-async fn serve(ctx: &mut Context<SyncChannel>, path: impl AsRef<Path>) -> roa::Result {
-    let dist = env::var("DIST_DIR")?;
-    let path = PathBuf::from(dist).join(path);
-    if !path.exists().await {
-        throw!(StatusCode::NOT_FOUND, format!("file not found"))
-    }
-    ctx.write_file(path, Inline).await
-}
-
-async fn index(ctx: &mut Context<SyncChannel>) -> roa::Result {
-    serve(ctx, "index.html").await
-}
-
-async fn serve_sources(ctx: &mut Context<SyncChannel>) -> roa::Result {
-    let file = ctx.must_param("file")?;
-    if !file.contains("..") && (file.ends_with("js") || file.ends_with("css")) {
-        serve(ctx, file.as_str()).await?;
-    }
-    throw!(
-        StatusCode::NOT_FOUND,
-        format!("file {} not found", file.as_str())
-    )
 }
